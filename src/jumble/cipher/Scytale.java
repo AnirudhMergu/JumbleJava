a72e705c342ee1dc50d3f74d4bd60cc0f7d483e4
ANIRUDH\Anirudh Mergu <anirudhsaim@gmail.com>
2018-04-29 00:29:47 +0530

    Added Scytale

--- src/jumble/cipher/Scytale.java
+++ src/jumble/cipher/Scytale.java
@@ -3,10 +3,17 @@
  * Original work by Anirudh Sai Mergu. All Rights Reserved.
  * Author Website: http://www.anirudhmergu.com
  * GitHub Repository: https://www.github.com/AnirudhMergu/JumbleJava
+ * Last updated on $date.long
  */
 
 package jumble.cipher;
 
 public class Scytale {
+    public String Encrypt(String text, int displacement) {
+        return "";
+    }
 
+    public String Decrypt(String cipher, int key) {
+        return "";
+    }
 }
