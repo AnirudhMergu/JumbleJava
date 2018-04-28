4aaadae2d53cf5e30947690ce7265bbc12e9c463
ANIRUDH\Anirudh Mergu <anirudhsaim@gmail.com>
2018-04-29 00:22:37 +0530

    Added Scytale

--- src/jumble/JumbleMain.java
+++ src/jumble/JumbleMain.java
@@ -1,8 +1,11 @@
-package jumble;
-
-/**
- * %Copyright%
+/*
+ * Copyright (c) 2018.
+ * Original work by Anirudh Sai Mergu. All Rights Reserved.
+ * Author Website: http://www.anirudhmergu.com
+ * GitHub Repository: https://www.github.com/AnirudhMergu/JumbleJava
  */
+
+package jumble;
 
 public class JumbleMain {
     public static void main(String[] args) {
--- src/jumble/cipher/Scytale.java
+++ src/jumble/cipher/Scytale.java
@@ -1,8 +1,12 @@
+/*
+ * Copyright (c) 2018.
+ * Original work by Anirudh Sai Mergu. All Rights Reserved.
+ * Author Website: http://www.anirudhmergu.com
+ * GitHub Repository: https://www.github.com/AnirudhMergu/JumbleJava
+ */
+
 package jumble.cipher;
 
-/**
- * %Copyright%
- */
 public class Scytale {
 
 }
